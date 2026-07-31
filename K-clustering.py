bd3dc0729003571047ad33f4676b8c1e0ee61c88
Geraldine Rodriguez Nieto <46820312+yerhi@users.noreply.github.com>
2026-07-31 16:10:47 +0200

    Update K-clustering.py

--- K-clustering.py
+++ K-clustering.py
@@ -3,24 +3,16 @@
 Created on Tue Sep 28 11:29:53 2021
 
 @author: u0136350
+
+Geraldine Rodriguez Nieto
 """
-
-
-# K-clustering
-
 import pandas as pd
-
 from sklearn.cluster import KMeans
-
 import matplotlib.pyplot as plt
 
 
 
 data = pd.read_csv('C:/Users/u0136350/Documents/KU_Brain/GABA/Metabolites_correctedByTissueComposition_woOutliersOron.csv', na_values='', sep=',')
-#data_m = pd.read_csv('G:/KU_Leuven/GABA/MetabolitesT.csv', na_values='', sep=';', decimal=",")
-
-
-data = pd.read_csv()
 
 wcss = []
 
@@ -36,8 +28,6 @@ plt.xlabel('Number of Clusters')
 plt.ylabel('WCSS')
 plt.title('K-means Clustering')
 plt.show() 
-
-#decidir el número de cluster de acuerdo a la regla del codo
 
 kmeans = KMeans(n_clusters=4, init='k-means++', random_state=42)
 kmeans.fit(segmentation_std)  
